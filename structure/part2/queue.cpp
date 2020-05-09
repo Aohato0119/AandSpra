4ca309e7ce78ff0a9107052e4faa7dbbb50385fc
Aohato <haruao1124@icloud.com>
2020-05-09 21:12:13 +0900

    S queue

--- structure/part2/queue.cpp
+++ structure/part2/queue.cpp
@@ -8,13 +8,44 @@ int main()
     int n, q ;
     std::cin >> n >> q ;
 
-    std::queue<std::pair<std::string, int>> que;
+    using process = std::pair<std::string, int> ;
+
+    std::queue<process> que;
     
-    std::pair<std::string, int> a ;
+    process a ;
     for(int i = 0 ; i != n ; ++i)
     {
-        cin >> a.first >> a.second ;
+        std::cin >> a.first >> a.second ;
         que.push( a ) ;
     }
+
+    int time{} ;
+    std::queue<process> timetable ;
+    
+    while(!que.empty())
+    {
+        a = que.front() ;
+        que.pop() ;
+        if(q < a.second)
+        {
+            time += q ;
+            a.second -= q ;
+            que.push(a) ;
+            
+        }
+        else
+        {
+            time += a.second ;
+            a.second = time ;
+            timetable.push(a) ;
+        }
+    }
+        
+    while(!timetable.empty())
+    {
+        std::cout << timetable.front().first << " " << timetable.front().second << std::endl ;
+        timetable.pop();
+    }
+
 }
 
